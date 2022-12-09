8481ea1c6c75260d734fa328b442b5db1c2270c4
Rafhael de Oliveira Martins <rafhaeldeoliveira@gmail.com>
2022-12-09 18:31:22 -0300

    👌 agora vai, bibliotecas.

--- speeling_nw_app.py
+++ speeling_nw_app.py
@@ -25,7 +25,7 @@ import numpy as np
 # from textblob import Word
 # from textblob import TextBlob
 
-# import nltk
+import nltk
 #nltk.download('stopwords')
 #stopwords = nltk.corpus.stopwords.words('portuguese')
 
